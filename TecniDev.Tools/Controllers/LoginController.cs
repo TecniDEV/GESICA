78ff6dcc450d0588a4cd6dc059d3d84803d678a0
Andrengineer <ndrengineer@gmail.com>
2023-11-21 17:12:39 -0500

    LoginController

--- TecniDev.Tools/Controllers/LoginController.cs
+++ TecniDev.Tools/Controllers/LoginController.cs
@@ -1,7 +1,50 @@
-﻿namespace TecniDev.Tools.Controllers
+﻿using TecniDev.Tools.Data.Context;
+using TecniDev.Tools.Data.Models;
+
+namespace TecniDev.Tools.Controllers
 {
     public class LoginController
     {
+        private readonly LoginContext Context = new();
 
+        public LoginController() 
+        { 
+           Context.Database.EnsureCreated();
+        }
+
+        public void Add(User user)
+        {
+            using (Context)
+            {
+                Context.Users.Add(user);
+                Context.SaveChanges();
+            }
+        }
+
+        public void Remove(User user)
+        {
+            using (Context)
+            {
+                Context.Users.Remove(user);
+                Context.SaveChanges();
+            }
+        }
+
+        public void Update(User user) 
+        { 
+            using (Context) 
+            {
+                Context.Users.Update(user);
+                Context.SaveChanges(); 
+            }
+        }
+
+        public ICollection<User> GetUsers() 
+        {
+            using(Context) 
+            {
+                return [.. Context.Users.ToList()];
+            }
+        }
     }
 }
